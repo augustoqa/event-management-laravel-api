412d96ab52f628a4bb70f94f0f22a1358b93f113
Augusto Quisquinay <augustoacual@gmail.com>
2024-12-06 18:53:14 -0600

    Store and Event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,12 +11,14 @@ class Event extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function attendees(): HasMany 
+    public function attendees(): HasMany
     {
         return $this->hasMany(Attendee::class);
     }
