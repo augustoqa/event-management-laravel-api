baf70d6a010b7d6086fcfc9a175b4e972a8715d6
Augusto Quisquinay <augustoacual@gmail.com>
2025-02-12 09:05:28 -0600

    Authorization with Gates

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -13,6 +13,11 @@ class AttendeeController extends Controller
 {
     use CanLoadRelationships;
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+    }
+
     private array $relations = ['user'];
 
     /**
@@ -54,8 +59,10 @@ class AttendeeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $event, Attendee $attendee)
+    public function destroy(Event $event, Attendee $attendee)
     {
+        $this->authorize('delete-attendee', [$event, $attendee]);
+
         $attendee->delete();
 
         return response(status: 204);
